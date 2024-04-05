8def92853c64ff6fd8d6082cfe50180b2f0fe91f
hyeily0627 <hj_o5h@naver.com>
2024-04-05 17:21:12 +0900

    2024-04-05

---
+++ day07/sql028_c8_s01_t1.sql
@@ -0,0 +1,20 @@
+--트랜잭션 1번 
+
+BEGIN TRANSACTION;
+USE madang;
+SELECT * 
+FROM   Book
+WHERE  bookid=1;
+
+ 
+UPDATE Book 
+SET     price=7100
+WHERE  bookid=1;
+
+SELECT * 
+FROM   Book
+WHERE  bookid=1;
+
+ 
+
+COMMIT;
